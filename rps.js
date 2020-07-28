7fef75ef03ec2921d91c872aba25b8003418eaf6
Sahil Malhotra <sahilm1990@gmail.com>
2020-07-29 01:06:53 +0530

    rock paper scissors with bonus and orchestration functions

--- rps.js
+++ rps.js
@@ -1,87 +1,93 @@
-function rockPaperScissors() {
-  const readline = require('readline-sync');
-  const VALID_CHOICES = [1, 2, 3];
-  function rps() {
-    function validUserInput(inp) {
-      while (!VALID_CHOICES.includes(inp)) {
-        console.log('please choose a valid input');
-        inp = parseInt(readline.question('Please enter your choice\n1 for Rock | 2 for Scissors | 3 for Paper: '), 10);
-      }
-    }
+const readline = require('readline-sync');
+const rpsMessages = require('./rpsMessages.json');
 
-    const userInput = parseInt(readline.question('Please enter your choice\n1 for Rock | 2 for Scissors | 3 for Paper: '), 10);
+const VALID_CHOICES = ['rock', 'paper', 'scissors', 'spock', 'lizard', 'x', 'l', 's', 'p', 'r'];
 
-    validUserInput(userInput);
-
-    switch (userInput) {
-      case 1:
-        console.log('You chose 1 - Rock');
-        break;
-      case 2:
-        console.log('You chose 2 - Scissors');
-        break;
-      case 3:
-        console.log('You chose 3 - Paper');
-        break;
-      default:
-        console.log('This is not a valid choice');
-    }
-
-    function getRandomInt(max) {
-      return Math.floor(Math.random() * Math.floor(max));
-    }
-    const computerInput = getRandomInt(3) + 1;
-    switch (computerInput) {
-      case 1:
-        console.log('Computer chose 1 - Rock');
-        break;
-      case 2:
-        console.log('Computer chose 2 - Scissors');
-        break;
-      case 3:
-        console.log('Computer chose 3 - Paper');
-        break;
-      default:
-        console.log("Computer couldn't choose a valid choice");
-    }
-    if (userInput === 1 && computerInput === 2) {
-      console.log('You win!');
-    } else if (userInput === 2 && computerInput === 1) {
-      console.log('You loose!');
-    } else if (userInput === 1 && computerInput === 3) {
-      console.log('You loose!');
-    } else if (userInput === 3 && computerInput === 1) {
-      console.log('You win!');
-    } else if (userInput === 2 && computerInput === 3) {
-      console.log('You win!');
-    } else if (userInput === 3 && computerInput === 2) {
-      console.log('You loose!');
-    } else if (userInput === computerInput) {
-      console.log("It's a tie");
-    }
-  }
-
-  rps();
-
-  function playAgain() {
-    function validPlayAgain(playAgain) {
-      return ['y', 'n'].includes(playAgain);
-    }
-
-    let playAgainUserInput = readline.question("would you like to play again? Please 'y' for yes, 'n' for no ").toLowerCase();
-
-    while (validPlayAgain(playAgainUserInput) === false) {
-      console.log('Please enter a valid choice');
-      playAgainUserInput = readline.question("would you like to play again? Please 'y' for yes, 'n' for no ").toLowerCase();
-    }
-
-    while (playAgainUserInput === 'y' || playAgainUserInput === 'yes') {
-      rps();
-      playAgainUserInput = readline.question("would you like to play again? Please 'y' for yes, 'n' for no ").toLowerCase();
-    }
-  }
-  playAgain();
-  console.clear();
+function computerInput() {
+  const computerChoice = rpsMessages[VALID_CHOICES[Math.floor(Math.random() * VALID_CHOICES.length)]];
+  console.log(`The computer chose ${computerChoice}`);
+  return computerChoice;
 }
 
-rockPaperScissors();
+function UserInput() {
+  let userInput = String(readline.question('Please enter your option: ').toLowerCase());
+
+  while (!VALID_CHOICES.includes(userInput)) {
+    console.log('This is not a valid input. Please enter your choice again');
+    console.log(rpsMessages.choices);
+    userInput = readline.question('Please enter your option: ').toLowerCase();
+  }
+
+  console.log(`Your choice is ${rpsMessages[userInput]}`);
+  return rpsMessages[userInput];
+}
+
+function userWins(choice, computerChoice) {
+  if ((choice === 'Rock' && computerChoice === 'Scissors')
+         || (choice === 'Rock' && computerChoice === 'Lizard')
+         || (choice === 'Paper' && computerChoice === 'Rock')
+         || (choice === 'Paper' && computerChoice === 'Spock')
+         || (choice === 'Scissors' && computerChoice === 'Paper')
+         || (choice === 'Scissors' && computerChoice === 'Lizard')
+         || (choice === 'Lizard' && computerChoice === 'Paper')
+         || (choice === 'Lizard' && computerChoice === 'Spock')
+         || (choice === 'Spock' && computerChoice === 'Rock')
+         || (choice === 'Spock' && computerChoice === 'Scissors')) {
+    return true;
+  }
+  if (choice === computerChoice) {
+    return 'tie';
+  }
+
+  return false;
+}
+
+function divider() {
+  console.log('-------------------------------------');
+}
+let userWinCount = 0;
+let computerWinCount = 0;
+let gameCount = 1;
+
+function rpsGame() {
+  console.log(`${rpsMessages.welcome} Game ${gameCount}\n`);
+  console.log(rpsMessages.choices);
+  const userChoice = UserInput();
+  const computerChoice = computerInput();
+  const result = userWins(userChoice, computerChoice);
+
+  if (result === true) {
+    userWinCount += 1;
+    console.log('You won!\n');
+  } else if (result === false) {
+    computerWinCount += 1;
+    console.log('You lose!\n');
+  } else {
+    console.log("It's a TIE!!\n");
+  }
+
+  console.log(`Total games played: ${gameCount} | Your score: ${userWinCount} | Computer's score: ${computerWinCount} | Games tied: ${gameCount - computerWinCount - userWinCount}\n`);
+  divider();
+  gameCount += 1;
+}
+
+let repeat = 'y';
+
+function playAgain(count) {
+  const validRepeat = ['y', 'yes'];
+  while (count <= 5 && validRepeat.includes(repeat)) {
+    if (count > 5) {
+      console.log('You\'ve played 5 games');
+    } else if (count === 5) {
+      rpsGame();
+      count += 1;
+      // console.log(count);
+    } else {
+      rpsGame();
+      repeat = String(readline.question('Would you like to play again? ')).toLowerCase();
+      count += 1;
+    }
+  }
+}
+
+playAgain(gameCount);
